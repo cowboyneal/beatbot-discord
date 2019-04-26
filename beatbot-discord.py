19a7e03fa4d98c40a4a1685aab39dca0002f3d1b
Jonathan Pater <pater@cowboyneal.org>
2019-04-26 17:05:29 +0000

    Fix embed footer

--- beatbot-discord.py
+++ beatbot-discord.py
@@ -102,13 +102,13 @@ class Beatbot(discord.Client):
         current_song = self.mpd.currentsong()
 
         response = discord.Embed(color=discord.Colour.dark_blue(),
+                url=config.EMBED_URL,
                 title=current_song['title'],
-                description=current_song['artist'] + "\n**" +
+                description=current_song['artist'] + "\n\n**" +
                 current_song['album'] + '**')
         response.set_thumbnail(url=config.IMAGE_URL +
                 str(current_song['id']))
-        response.set_footer(text='[' + config.FOOTER_URL + '](' +
-                config.EMBED_URL + ')')
+        response.set_footer(text=config.FOOTER_URL)
 
         await message.channel.send(embed=response)
 
